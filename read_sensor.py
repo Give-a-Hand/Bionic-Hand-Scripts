6a14696ba447cbc7548cbb6342567e4f60e2021f
Ari Pant <59593062+pantari76@users.noreply.github.com>
2024-02-14 19:40:54 -0500

    Adding initial scripts

---
+++ read_sensor.py
@@ -0,0 +1,17 @@
+import csv
+from time import time
+import serial
+
+# Your serial port might be different!
+ser = serial.Serial('/dev/cu.usbmodem141301', timeout=1)
+f = open("df.csv", "a+")
+writer = csv.writer(f, delimiter=',')
+while True:
+    s = ser.readline().decode()
+    if s != "":
+        rows = [float(x) for x in s.split(',')]
+        # Insert local time to list's first position
+        rows.insert(0, int(time()))
+        print(rows)
+        writer.writerow(rows)
+        f.flush()
