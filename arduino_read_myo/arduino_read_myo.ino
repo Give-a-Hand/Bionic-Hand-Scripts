49c3d092d2facf2374b7448db4a99815610536fd
therealdeal201 <67665796+therealdeal201@users.noreply.github.com>
2024-02-28 19:58:25 -0500

    Yay!

---
+++ arduino_read_myo/arduino_read_myo.ino
@@ -0,0 +1,16 @@
+void setup()
+{
+  Serial.begin(9600);
+  while (!Serial); // optionally wait for serial terminal to open
+  Serial.println("MyoWare Example_01_analogRead_SINGLE");
+}
+void loop()
+{
+  int sensorValue = analogRead(A0); // read the input on analog pin A0
+  Serial.print(500);
+  Serial.print(", ");
+  Serial.print(0);
+  Serial.print(", ");
+  Serial.println(sensorValue); // print out the value you read
+  delay(50); // to avoid overloading the serial terminal
+}
