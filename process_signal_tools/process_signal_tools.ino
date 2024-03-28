1b1bdbbbe8f3cd63d85179833e1581e48db1c40a
Ari Pant <pantari76@gmail.com>
2024-03-27 20:40:20 -0400

    merge motor movement with sensor

--- process_signal_tools/process_signal_tools.ino
+++ process_signal_tools/process_signal_tools.ino
@@ -4,14 +4,30 @@ int signalVals[20] = {};
 int signalValsSize = 20; 
 
 int rawPrevious = 0;
-int index = 0;
+bool clenched = false;
+
+bool input;
+int count = 0;
+
+int button = 13;
+int motor_cw = 12;
+int motor_ccw = 11;
 
 void setup() {
   Serial.begin(9600);
+
+  pinMode(button, INPUT);
+  pinMode(motor_cw, OUTPUT);
+  pinMode(motor_ccw, OUTPUT);
+
+  Serial.begin(9600);
+
+
   while (!Serial); // wait for serial terminal to open
 }
 
 void loop() {
+  
   // read the input on analog pin A0
   int rawValue = analogRead(A0); 
 
@@ -30,6 +46,39 @@ void loop() {
   //Serial.print(", ");
   Serial.println(currentSTD); 
 
+  /* 
+    The determines if there has been a state change
+  */
+
+  if (rawValue > 100 && currentSTD > 10) {
+    clenched = !clenched;
+    motorMoving = true;
+
+    if (clenched == HIGH) {
+      digitalWrite(motor_cw, LOW);
+      digitalWrite(motor_ccw, LOW);
+
+      count++;
+
+      if (count % 2 == 0) {
+        digitalWrite(motor_cw, HIGH);
+        digitalWrite(motor_ccw, LOW);
+      }
+      
+      if (count % 2 == 1) {
+        digitalWrite(motor_cw, LOW);
+        digitalWrite(motor_ccw, HIGH);
+      }
+      
+      delay(1000);
+      digitalWrite(motor_cw, LOW);
+      digitalWrite(motor_ccw, LOW);
+    }
+
+    delay(100);
+
+
+  }
   delay(50); // to avoid overloading the serial terminal
 }
 
@@ -53,4 +102,8 @@ double calculateStandardDeviation(int arr[])
     // square root of the variance to get the standard deviation 
     return sqrt(standardDeviation / signalValsSize); 
 } 
+
+
+
+
 
