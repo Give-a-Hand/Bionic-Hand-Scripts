1ea8e4f6ede12b39ba351899707c67e21968edeb
Vivaan Lunkad <lunkad.v@northeastern.edu>
2025-02-06 18:19:38 -0500

    Recording in Miliseconds

--- python_scripts/read_sensor.py
+++ python_scripts/read_sensor.py
@@ -3,16 +3,16 @@ from time import time
 import serial
 
 # Your serial port might be different!
-ser = serial.Serial('com4', timeout=1)
-f = open("lk_pressed.csv", "a+", newline='')
-writer = csv.writer(f, delimiter=',')
+ser = serial.Serial("com4", timeout=1)
+f = open("lk_pressed.csv", "a+", newline="")
+writer = csv.writer(f, delimiter=",")
 while True:
-    s = ser.readline().decode('utf-8')
+    s = ser.readline().decode("utf-8")
     if s != "":
-        rows = [float(str(x).replace('\n', '')) for x in s.split(',')]
+        rows = [float(str(x).replace("\n", "")) for x in s.split(",")]
         # Insert local time to list's first position
-        rows.insert(0, int(time()))
+        rows.insert(0, round(time.time() * 1000))
         print(rows)
 
         writer.writerow(rows)
-        f.flush() 
+        f.flush()
